1b1a9dda8cb8d58f657424e8330e18e1c71b14eb
thedemonsid <siddheshshrirame@gmail.com>
2024-01-04 11:54:31 +0530

    Add color buttons and background color change

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
-
+function Button({ color }) {
+  return (
+    <div className="flex justify-around ">
+      <button
+        id="blue"
+        className={`bg-${color}-600 m-1 rounded-lg py-2 px-4 text-white `}
+        onClick={() => setColor(color)}
+      >
+        {color}
+      </button>
+    </div>
+  );
+}
 function App() {
-  const [count, setCount] = useState(109);
+  const [color, setColor] = useState("blue");
 
   return (
     <>
-      <div>{count}</div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        Incrementor
-      </button>
-      <button onClick={() => setCount((count) => count - 1)}>
-        Decrementor
-      </button>
+      <div className={`fixed inset-x-0 top-0 bottom-[5%]  bg-${color}-600`}></div>
+      <div className="flex items-end justify-center min-h-screen z-10">
+        <div className="flex justify-center">
+          <Button color={"green"}></Button>
+          <Button color={"blue"}></Button>
+          <Button color={"red"}></Button>
+        </div>
+      </div>
     </>
   );
 }
