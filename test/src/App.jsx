3d4d2a5bd8374982551867d9baa701fe0d3e9437
thedemonsid <siddheshshrirame@gmail.com>
2024-01-04 12:00:35 +0530

    Refactor button component and add background color state
    This commit refactors the `Button` component in `App.jsx` to include an `onClick` prop and updates the usage of the component in the `App` component to pass the `onClick` function. Additionally, it renames the `color` state variable to `bgcolor` in the `App` component and updates the corresponding class names to use the `bgcolor` state value for the background color.

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
-function Button({ color }) {
+
+function Button({ color, onClick }) {
   return (
     <div className="flex justify-around ">
       <button
-        id="blue"
+        id={color}
         className={`bg-${color}-600 m-1 rounded-lg py-2 px-4 text-white `}
-        onClick={() => setColor(color)}
+        onClick={onClick}
       >
         {color}
       </button>
     </div>
   );
 }
-function App() {
-  const [color, setColor] = useState("blue");
 
+function App() {
+  const [bgcolor, setColor] = useState("blue");
   return (
     <>
-      <div className={`fixed inset-x-0 top-0 bottom-[5%]  bg-${color}-600`}></div>
+      <div
+        className={`fixed inset-x-0 top-0 bottom-[5%] bg-${bgcolor}-600`}
+      ></div>
       <div className="flex items-end justify-center min-h-screen z-10">
         <div className="flex justify-center">
-          <Button color={"green"}></Button>
-          <Button color={"blue"}></Button>
-          <Button color={"red"}></Button>
+          <Button color={"green"} onClick={() => setColor("green")}></Button>
+          <Button color={"blue"} onClick={() => setColor("blue")}></Button>
+          <Button color={"red"} onClick={() => setColor("red")}></Button>
         </div>
       </div>
     </>
