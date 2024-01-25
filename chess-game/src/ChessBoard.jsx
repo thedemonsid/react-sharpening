677b75412d12826f0eddb9800f5cbe5d8f74c0b3
thedemonsid <siddheshshrirame@gmail.com>
2024-01-25 12:45:38 +0530

    Refactor chess game logic for turns and piece movements and killing

--- chess-game/src/ChessBoard.jsx
+++ chess-game/src/ChessBoard.jsx
@@ -1,20 +1,23 @@
 import { Row, initialBoardState } from "./BoardComponent";
 import { useState } from "react";
 import * as PossibleMoves from "./PossibleMoves";
-
+// For turns as "turn" decides which chance is now
+let index = 0;
 const ChessBoard = () => {
   const [board, setBoard] = useState(initialBoardState);
   const [previousMove, setPreviousMove] = useState();
 
+  //Represents the current turn in the chess game.
+  let turn = ["w", "b"];
   const handleClick = (row, col, piece) => {
     console.log({ column: col, row: row, piece: piece });
 
     //chesks if there is prevmove and if current piece is not empty
-    if (previousMove && previousMove.piece && piece == null) {
+    if (previousMove && previousMove.piece) {
       let pieceType = previousMove.piece.split("-")[1];
       let color = previousMove.piece.split("-")[0];
       let possMoves;
-
+      console.log(index);
       switch (pieceType) {
         case "pawn":
           possMoves = PossibleMoves.getPawnMoves(
@@ -55,17 +58,35 @@ const ChessBoard = () => {
         default:
           return;
       }
-
-      possMoves.some((move) => {
-        if (move.column === col && move.row === row) {
-          let newBoard = [...board];
-          newBoard[previousMove.row][previousMove.column] = null;
-          newBoard[row][col] = previousMove.piece;
-          setBoard(newBoard);
-          return true;
-        }
-        return false;
-      });
+      // console.log(possMoves)
+      //Checking if the right player is playing
+      if (turn[index % 2] == color && piece == null) {
+        possMoves.some((move) => {
+          if (move.column === col && move.row === row) {
+            let newBoard = [...board];
+            newBoard[previousMove.row][previousMove.column] = null;
+            newBoard[row][col] = previousMove.piece;
+            setBoard(newBoard);
+            return true;
+          }
+          return false;
+        });
+        index++;
+      }
+      // killing mechanism hahahahahahahhahahaha
+      if (turn[index % 2] == color && piece != null && piece[0] != color) {
+        possMoves.some((move) => {
+          if (move.column === col && move.row === row) {
+            let newBoard = [...board];
+            newBoard[previousMove.row][previousMove.column] = null;
+            newBoard[row][col] = previousMove.piece;
+            setBoard(newBoard);
+            return true;
+          }
+          return false;
+        });
+        index++;
+      }
     }
 
     setPreviousMove({ row, column: col, piece });
